e5f1826d2a91f2f6c1d734c1b64c759267438014
AbdulazizQ3382 <abuqannam2@gmail.com>
2022-11-03 14:51:05 +0300

    whatever

--- search-engine/indexing.js
+++ search-engine/indexing.js
@@ -1,3 +1,4 @@
+
 /**
  * Generate a front-matter object from all markdowns,
  * and cache them into an indexes array.
@@ -5,6 +6,15 @@
  * @author Nawaf Khalifah
  * @contibuter Abdulaziz Qannam
  */
+
+
+
+ const express = require('express')
+ const app = express()
+ const port = 3000 
+ let cors = require('cors')
+ app.use(cors())
+
 const parseMarkdown = require('front-matter-markdown/lib')
 const path = require("path")
 const {readdirSync, readFileSync, writeFile} = require('fs')
@@ -12,6 +22,30 @@ const {readdirSync, readFileSync, writeFile} = require('fs')
 // Paths
 const DESTINATION_DIRECTORY = path.resolve(__dirname, '../site/content/docs/1.0')
 const DESTINATION_EXTENSION = '.md'
+
+
+app.post('/search', (req, res) => {
+  console.log(req.body.keyword);
+let rawdata = fs.readFileSync('/storage/indexes.json');
+let words = JSON.parse(rawdata);
+console.log(words);
+})
+
+const markdownParser = (text) => {
+  const toHTML = text
+    .replace(/^### (.*$)/gim, '<h3>$1</h3>') // h3 tag
+    .replace(/^## (.*$)/gim, '<h2>$1</h2>') // h2 tag
+    .replace(/^# (.*$)/gim, '<h1>$1</h1>') // h1 tag
+    .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>') // bold text
+    .replace(/\*(.*)\*/gim, '<i>$1</i>') // italic text
+    .replace(/<[^>]*>?/gm, "")
+    .replace(/`[^`]*`?/gm,"")
+    .replace(/({)[^(})]*(})?/gm,"")
+    .replace(/^(\{\{\< \w*([\W\w]) \>\}\})\s*((?:.|\s)*)+(\{\{\< \/\w*([\W\w]) \>\}\})/gm, '')
+    .replace(/[^a-zA-Z ]+/gm,"");
+  return toHTML.trim(); // using trim method to remove whitespace
+}
+
 
 // Finder
 const findFiles = (destinationDirectory) => {
@@ -35,6 +69,15 @@ const findFiles = (destinationDirectory) => {
   return files;
 }
 
+const onlyLetters = (str) => {
+  let words = str.split(' ');
+  for(let i=0; i<words.length;i++){
+    words[i] = words[i].replace(/[^a-zA-Z]+/,'')
+    console.log(words[i])
+  }
+  return words.join();
+}
+
 const files = findFiles(DESTINATION_DIRECTORY)
 let matters = [];
 
@@ -44,9 +87,11 @@ for (const file in files) {
 
   if (fileContent.length) {
     const stringify = fileContent.toString()
-    let frontMatter = parseMarkdown(stringify, {content : true, headingsAsToc: true})
+
+    let frontMatter = parseMarkdown(stringify, {content : true, headingsAsToc: false})
     frontMatter.id = files[file].name
-    frontMatter.Level2Contents = frontMatter.$compiled;
+    frontMatter.fullContent = markdownParser(frontMatter.content);
+
     matters.push(frontMatter)
   }
 
@@ -62,3 +107,8 @@ if (matters.length) {
     }
   )
 }
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ search-engine/ranker.js
@@ -0,0 +1,6 @@
+/**
+ * Rank range 1 ---> 5
+ * 3 Point if the title contain the search keyword
+ * 1.5 Point if the keyword repeated on the content one time.
+ * 2 Points if the keyword repeated twice or more on the content.
+ */
